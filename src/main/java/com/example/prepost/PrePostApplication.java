9e4dc6b8b6fbe9ce2358aaac9f0440f16b77d793
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-05-21 14:28:57 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/prepost/PrePostApplication.java
@@ -0,0 +1,13 @@
+package com.example.prepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrePostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prepost/PrePostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
